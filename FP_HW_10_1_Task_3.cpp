ec4207240f1146f5be742950110fc87893a4bbbc
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-19 14:40:39 +0300

    Версия 0.1

--- FP_HW_10_1_Task_3.cpp
+++ FP_HW_10_1_Task_3.cpp
@@ -11,67 +11,68 @@
 //    0 1 4 5 3 9
 
 #include <iostream>
-#include <windows.h>
 #include <conio.h>
 #include <random>
+
 using namespace std;
 
-void FillArray(int array[][15], int size);
+const int rows = 15;
+const int cols = 15;
+int Array[rows][cols];
 
-int main()
-{
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+void FillArray(int array[][cols], int size);
+void OffsetToLeft(int array[][cols], int offset, int size);
+void OffsetToRight(int array[][cols], int offset, int size);
+void PrintArray(int array[][cols], int size);
 
+int main() {
     char pressedButton;
 
-    const int sizeArr = 15;    
-    int Array[sizeArr][sizeArr];
+    FillArray(Array, rows);
 
-    FillArray(Array, sizeArr);
-
-    cout << "Для управления программой используйте стрелочки (вперёд, назад, влево, вправо). Для выхода нажать esc" << std::endl;
+    cout << "Для управления программой используйте стрелочки (вперёд, назад, влево, вправо). Для выхода нажмите esc" << std::endl;
 
     while (true) {
         if (_kbhit()) {
             pressedButton = _getch();
-            
-            if (pressedButton == -32 || pressedButton == 0) { 
-                pressedButton = _getch();  
+
+            if (pressedButton == -32 || pressedButton == 0) {
+                pressedButton = _getch();
 
                 switch (pressedButton) {
                 case 72:
-                    cout << "Up arrow pressed" << std::endl;
+                    OffsetToRight(Array, 1, rows);
                     break;
                 case 80:
-                    cout << "Down arrow pressed" << std::endl;
+                    OffsetToLeft(Array, 1, rows);
                     break;
                 case 75:
-                    cout << "Left arrow pressed" << std::endl;
+                    OffsetToLeft(Array, 2, rows);
                     break;
                 case 77:
-                    cout << "Right arrow pressed" << std::endl;
+                    OffsetToRight(Array, 2, rows);
                     break;
                 default:
                     break;
                 }
             }
-                        
+
             if (pressedButton == 27) {
                 break;
             }
-        }       
-    }   
+
+            PrintArray(Array, rows);
+        }
+    }
+
+    return 0;
 }
 
-void FillArray(int array[][15], int size) 
-{
+void FillArray(int array[][cols], int size) {
     random_device Random;
 
-    for (int x = 0; x < size; x++)
-    {
-        for (int y = 0; y < size; y++)
-        {
+    for (int x = 0; x < size; x++) {
+        for (int y = 0; y < size; y++) {
             array[x][y] = Random() % 100;
 
             cout << array[x][y] << "\t";
@@ -79,5 +80,39 @@ void FillArray(int array[][15], int size)
 
         cout << endl;
     }
+}
 
+void PrintArray(int array[][cols], int size) {
+    system("cls"); 
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            cout << array[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+void OffsetToLeft(int array[][cols], int offset, int size) {
+    for (int x = 0; x < size; x++) {
+        for (int i = 0; i < offset; i++) {
+            int temp = array[x][0];
+            for (int y = 0; y < size - 1; y++) {
+                array[x][y] = array[x][y + 1];
+            }
+            array[x][size - 1] = temp;
+        }
+    }
+}
+
+void OffsetToRight(int array[][cols], int offset, int size) {
+    for (int x = 0; x < size; x++) {
+        for (int i = 0; i < offset; i++) {
+            int temp = array[x][size - 1];
+            for (int y = size - 1; y > 0; y--) {
+                array[x][y] = array[x][y - 1];
+            }
+            array[x][0] = temp;
+        }
+    }
 }
